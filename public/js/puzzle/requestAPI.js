e2864d5d0c0930c5907e69fd248de59f03639bbf
jona <j.l.van.gessel@student.vu.nl>
2018-06-12 13:06:02 +0200

    new changes

--- public/js/puzzle/requestAPI.js
+++ public/js/puzzle/requestAPI.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
     $.get("https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=SGSqBgrf&format=json", function (status) {
-        alert("ja");
+        alert("lekkergeladenpik");
     });
 });
