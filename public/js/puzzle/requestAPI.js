36b58ced3bdc9e779502aee0d07f98b7e5693d6d
mike <rijndersmike@gmail.com>
2018-06-13 09:13:07 +0200

    linkjes weg

--- public/js/puzzle/requestAPI.js
+++ public/js/puzzle/requestAPI.js
@@ -1,9 +1,19 @@
+// $( document ).ready(function() {
+//     $.get("https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=SGSqBgrf&format=json", function (status) {
+//         var data = '';
+//         $.each(status, function(key, value){
+//             data += value.description
+//         });
+//         alert(data);
+//     });
+// });
+
 $( document ).ready(function() {
-    $.get("https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=SGSqBgrf&format=json", function (status) {
-        var data = '';
+    $.get("https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=SGSqBgrf&format=json", function (image) {
+        var photo = '';
         $.each(status, function(key, value){
-            data += value.description
+            photo += '<img src='+value.webImage.url+'>'
         });
-        alert(data);
+        $('#image').append(photo);
     });
 });
