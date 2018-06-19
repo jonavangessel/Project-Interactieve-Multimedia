3b014b6631938bb59a3285609b224b231496d5f7
mike <rijndersmike@gmail.com>
2018-06-19 15:15:55 +0200

    linkjes weg

--- public/js/puzzle/requestAPI.js
+++ public/js/puzzle/requestAPI.js
@@ -10,30 +10,36 @@
 //     });
 // });
 
-$( document ).ready(function() {
-    $("#info").click(function () {
-        $.get("https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=SGSqBgrf&format=json", function (status) {
-            var dataTitle = '';
-            var dataDescription = '';
-            $.each(status, function (key, value) {
-                dataDescription += value.description;
-                dataTitle += value.title;
-            });
-            alert(dataTitle);
-            $('#info').attr({
-                "title": dataTitle,
-                "data-content": dataDescription
-            });
+function klik() {
+    $.get("https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=SGSqBgrf&format=json", function (status) {
+        var dataTitle = '';
+        var dataDescription = '';
+        $.each(status, function (key, value) {
+            dataDescription += value.description;
+            dataTitle += value.titles;
         });
+        document.getElementById("bij").setAttribute("title", dataTitle);
+        document.getElementById("bij").setAttribute("data-content", dataDescription)
     });
-});
+}
 
-// $( document ).ready(function() {
+
+
+// $(document).ready(function() {
 //     $.get("https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=SGSqBgrf&format=json", function (image) {
-//         var photo = '';
-//         $.each(image, function(key, value){
-//             photo += value.webImage.url
-//         });
-//         $('#image').append(photo);
+//         var photo = 'http://lh3.googleusercontent.com/J-mxAE7CPu-DXIOx4QKBtb0GC4ud37da1QK7CzbTIDswmvZHXhLm4Tv2-1H3iBXJWAW_bHm7dMl3j5wv_XiWAg55VOM=s0';
+//         document.getElementById("hello").setAttribute("src", photo)
 //     });
 // });
+
+// if (slidingPuzzle.isSolved() === true) {
+//     $.get("https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=SGSqBgrf&format=json", function (status) {
+//         var dataTitle = '';
+//         var dataDescription = '';
+//         $.each(status, function (key, value) {
+//             dataDescription += value.description;
+//             dataTitle += value.titles;
+//         });
+//         alert("yehes")
+//     });
+// }
